cf7a55684b6e37fce3b73803fbf8e20a82d54aea
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-01 20:23:21 +0100

    feat: add UpdateScore for updating scores

---
+++ src/components/UpdateScore.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+      <h1>Update Scores</h1>
+  
+      <!-- Team Dropdown -->
+      <label for="team">Select Team:</label>
+      <select v-model="selectedTeam" class="team">
+        <option v-for="team in teams" :key="team">{{ team }}</option>
+      </select>
+  
+      <!-- Score Input -->
+      <label for="score">Enter Score:</label>
+      <input v-model="score" type="number" class="score" />
+  
+      <!-- Update Button -->
+      <button @click="updateScore">Update Score</button>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    data() {
+      return {
+        selectedTeam: null,
+        score: null,
+        teams: ['Astralis', 'NiP', 'NAVI'],
+      };
+    },
+    methods: {
+      updateScore() {
+        console.log('Updating score:', this.selectedTeam, this.score);
+      },
+    },
+  };
+  </script>
+  
+  <style scoped>
+  
+  </style>
+  
