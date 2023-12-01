12aa4fa0cfa55fae8f47458c70ba1896c13db970
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-01 20:41:50 +0100

    style: add general styling

--- src/components/UpdateScore.vue
+++ src/components/UpdateScore.vue
@@ -4,8 +4,8 @@
 
         <!-- Team Dropdown -->
         <div class="team">
-            <label for="team">Select Team:</label>
-            <select v-model="selectedTeam" class="team-selector">
+            <label for="team">Select Team</label>
+            <select v-model="selectedTeam" class="selector">
                 <option v-for="team in teams" :key="team">{{ team }}</option>
             </select>
         </div>
@@ -13,8 +13,8 @@
 
         <!-- Score Input -->
         <div class="score">
-            <label for="score">Enter Score:</label>
-            <input v-model="score" type="number" class="score-input" />
+            <label for="score">Enter Score</label>
+            <input v-model="score" type="number" class="selector" />
         </div>
 
         <!-- Update Button -->
@@ -40,5 +40,36 @@ export default {
 </script>
   
 <style scoped>
+h1 {
+    font-size: 4em;
+    margin-bottom: 0.5em;
+}
+.frame {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 98vh;
+}
+
+.team {
+  margin-bottom: 1em;
+}
+
+.selector {
+    margin-left: 0.5em;
+}
+
+button {
+    margin-top: 2em;
+    padding: 0.5em 1em;
+    border-radius: 0.2em;
+    border: none;
+    background-color: greenyellow;
+    color: black;
+    font-size: 1em;
+    text-transform: uppercase;
+    cursor: pointer;
+}
 </style>
   
