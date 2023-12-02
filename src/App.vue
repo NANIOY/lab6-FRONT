9ec46cdc41278e89ebd41b05a345fc39c319e72d
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-02 16:52:06 +0100

    test: add scoreboard

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
   import UpdateScore from './components/UpdateScore.vue';
+  import Scoreboard from './components/Scoreboard.vue';
 </script>
 
 <template>
   <div>
     <UpdateScore />
+    <Scoreboard />
   </div>
 </template>
 
