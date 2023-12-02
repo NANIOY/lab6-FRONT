8e798e39ee33d759c05e6c1263249203fd38b1f7
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-02 17:29:12 +0100

    feat: add router-links + style them

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
-<script setup>
-  import UpdateScore from './components/UpdateScore.vue';
-  import Scoreboard from './components/Scoreboard.vue';
-</script>
-
 <template>
-  <div>
-    <UpdateScore />
-    <Scoreboard />
+  <div id="app">
+    <nav>
+      <router-link to="/update" class="nav-link">Update Scores</router-link>
+      <router-link to="/scoreboard" class="nav-link">Scoreboard</router-link>
+    </nav>
+    <router-view />
   </div>
 </template>
 
-<style scoped>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+}
 
+.nav-link {
+  margin: 24px;
+  font-size: 1.5em;
+  text-decoration: none;
+  color: #7a7a7a;
+}
+
+.nav-link.router-link-exact-active {
+  color: greenyellow;
+  font-weight: bold;
+}
 </style>
