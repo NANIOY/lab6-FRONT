3ef7a7c936d76bee8f7ba2a62f949e488137dff7
Yannick Van den Brande <yavadebr@gmail.com>
2023-12-02 20:34:29 +0100

    feat: add websocket integration in vue app

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-import './style.css'
+import './style.css';
 
-createApp(App).use(router).mount('#app');
+const socket = new WebSocket('wss://lab6-back.onrender.com/primus');
+
+socket.addEventListener('open', (event) => {
+  console.log('WebSocket connection opened:', event);
+});
+
+socket.addEventListener('message', (event) => {
+  console.log('WebSocket message received:', event);
+  const data = JSON.parse(event.data);
+});
+
+const app = createApp(App);
+app.config.globalProperties.$socket = socket;
+app.use(router);
+app.mount('#app');
